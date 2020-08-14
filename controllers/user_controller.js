ac209c7f1c7289c2679988b3a14048360e97c586
Archana <archysimats@gmail.com>
2020-08-14 10:31:40 -0700

    commit before starting passport js

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,11 +1,22 @@
 const User = require('../models/user');
-
 module.exports.profile = function(req,res){
-    //res.end('<h1>User Profile</h1>');
-
-    return res.render('user_profile',{
-        title: "User profile"
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile',{
+                    title : "user_profile",
+                    user : user
+                })
+            }
+            else{
+                return res.redirect('/user/sign-in');
+            }
+           
+        });
+    }
+            else{
+                return res.redirect('/user/sign-in');
+            }
 }
 // render the sign up and sign in page
 module.exports.signUp = function(req,res){
@@ -40,4 +51,31 @@ module.exports.create = function(req,res){
             return res.redirect('back');
         }
     }) ;   
+}
+
+module.exports.createSession = function(req,res){
+
+    //find the user
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing up');return;}
+
+    //handle user found
+     if(user){
+        if(user.password != req.body.password){
+            return res.redirect('back');
+            }
+
+    // handle password which doenot match
+
+     res.cookie('user_id',user.id);
+     return res.redirect('/user/profile');
+
+    // handle session creation
+     
+    } else{
+        return res.redirect('back');
+    }   
+    // handle user not found
+});
 }
