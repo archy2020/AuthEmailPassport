3867f872b43e9b1cbc683da4fd9af4a0a27df1d6
Archana <archysimats@gmail.com>
2020-08-27 17:24:38 -0700

    database create update delete read done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,31 +1,50 @@
 const User = require('../models/user');
 const Post = require('../models/post');
 
-module.exports.home = function(req,res){
 
-//     post.find({}, function(err,post){
-//     return res.render('home',{
-//         title :"Email Authorization | home",
-//         post : post
-//     });
-// });
+module.exports.home = async function(req,res){
 
-Post.find({})
-.populate('user')
-.populate({
-    path : 'comments',
-    populate : {
-        path : 'user'
-    }
-})
-.exec(function(err,posts){
-    
-    User.find({}, function(err, user){
-        return res.render('home', {
-            title :"Email Authorization | home",
-            posts : posts,
-            all_users : user
-        });
+ try{
+    //populate the user of each post
+   let posts = await Post.find({})
+    .populate('user')
+    .populate({
+        path : 'comments',
+        populate : {
+            path : 'user'
+        }
     });
-})
-}
+    
+        
+    let users = await    User.find({});
+
+            return res.render('home', {
+                title :"Email Authorization | home",
+                posts : posts,
+                all_users : users
+            });
+        } catch(err){
+            console.log('Error',err);
+            return;
+        }
+    }       
+// module.exports.home = function(req,res){
+// Post.find({})
+// .populate('user')
+// .populate({
+//     path : 'comments',
+//     populate : {
+//         path : 'user'
+//     }
+// })
+// .exec(function(err,posts){
+    
+//     User.find({}, function(err, user){
+//         return res.render('home', {
+//             title :"Email Authorization | home",
+//             posts : posts,
+//             all_users : user
+//         });
+//     });
+// })
+// }
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const port = 8000;
-
 const app = express();
-// layout
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 //used for session cookie
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
 const MongoStore = require('connect-mongo')(session);
 const saasMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 app.use(saasMiddleware({
     src : './assets/scss',
@@ -58,8 +57,9 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(passport.setAuthenticatedUser);
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router
 app.use('/',require('./routes'));
